ceeb262ecc87da3e30b641ef61b8ce1272811b84
Jhon Edinson Luna Santos <jhonlunaa56@gmail.com>
2023-03-08 18:34:50 -0500

    scroll_ok

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,14 +1,23 @@
+import scrollTopButton from "./dom/boton_scroll.js";
+import countdown from "./dom/cuenta_regresiva.js";
 import hamburgerMenu from "./dom/menu_hamburgues.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
 
 const d = document;
 
-    d.addEventListener("DOMContentLoaded", (e) =>{
+d.addEventListener("DOMContentLoaded", (e) =>{
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
-    alarm("assets/alarma.mp3","#activar-alarma", "#desactivar-alarma" )
+    alarm("assets/alarma.mp3","#activar-alarma", "#desactivar-alarma" ),
+    countdown(  
+    "countdown",
+    "June 09, 2023 00:00:00",
+    "Feliz Cumpleaños Jhon Luna Santos");
+
+    scrollTopButton(".scroll-top-btn")
 });
+
 
 
 d.addEventListener("keydown", (e) =>{

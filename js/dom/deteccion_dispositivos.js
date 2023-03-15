f358ce2b44a8177d49ae535398f0d283359fe600
Jhon Edinson Luna Santos <jhonlunaa56@gmail.com>
2023-03-14 22:40:46 -0500

    okdeteccion

--- js/dom/deteccion_dispositivos.js
+++ js/dom/deteccion_dispositivos.js
@@ -2,4 +2,43 @@ const d = document,
 n = navigator,
 ua = n.userAgent;
 export default function userDeviceInfo(id){
+    const $id = d.getElementById(id),
+    isMobile = {
+        android: ()=>ua.match(/android/i),
+        ios: ()=>ua.match(/iphone/i),
+        windows: ()=>ua.match(/windows phone/i),
+        any: function (){
+            return this.android()|| this.ios()|| this.windows();
+        },
+    },
+    isDesktop = {
+        linux: ()=>ua.match(/linux/i),
+        mac: ()=>ua.match(/mac os/i),
+        windows: ()=> ua.match(/windows nt/i),
+        any: function (){
+            return this.mac()|| this.windows() ||this.linux();
+        }
+    },
+    isBrowser = {
+        chrome: ()=>ua.match(/chrome/i),
+        safari: ()=>ua.match(/safari/i),
+        firefox: ()=>ua.match(/firefox/i),
+        edge: ()=>ua.match(/edge/i),
+        any: function (){
+            return (
+                this.safari()||
+                this.firefox()||
+                this.edge()||
+                this.chrome()
+            )
+        }
+    };
+    $id.innerHTML = `
+    <ul>
+    <li>User Agent: <b>${ua}</b></li>
+    <li>Plataforma: <b>${
+        isMobile.any()? isMobile.any():isDesktop.any()
+    }</b></li>
+    <li>Navegador: <b>${isBrowser.any()}</b></li>
+    </ul>`;
 }

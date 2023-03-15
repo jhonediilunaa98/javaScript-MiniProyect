21e87dc600e0502d4adb7a7de54fafaf6c461a75
Jhon Edinson Luna Santos <jhonlunaa56@gmail.com>
2023-03-14 22:42:51 -0500

    okok

--- js/dom/deteccion_dispositivos.js
+++ js/dom/deteccion_dispositivos.js
@@ -5,7 +5,7 @@ export default function userDeviceInfo(id){
     const $id = d.getElementById(id),
     isMobile = {
         android: ()=>ua.match(/android/i),
-        ios: ()=>ua.match(/iphone/i),
+        ios: ()=>ua.match(/iphone|ipad|ipod/i),
         windows: ()=>ua.match(/windows phone/i),
         any: function (){
             return this.android()|| this.ios()|| this.windows();
